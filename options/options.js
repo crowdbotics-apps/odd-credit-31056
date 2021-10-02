fe33694b35b5574279ef4ecc1abae60084f6a87e
Crowdbotics <team@crowdbotics.com>
2021-10-02 23:18:08 +0000

    Initial commit.

---
+++ options/options.js
@@ -0,0 +1,12 @@
+import { StyleSheet } from "react-native";
+
+export const globalOptions = {
+  name: "odd_credit_31056",
+  url: "https://odd_credit_31056.botics.co",
+  api: "https://odd_credit_31056.botics.co/api/v1"
+}
+export const modulesOptions = {
+  "@modules/app-menu": {
+    "copy": "Routes available!"
+  }
+}
